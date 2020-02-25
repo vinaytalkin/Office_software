3a94e0ff3f930602ae1dce0bbcb2d9b8bff32852
vinay kumar <vinaytalkin@yahoo.com>
2020-02-25 13:36:28 +0530

    changed the style using material

--- Truni/Office_software/center/form.py
+++ Truni/Office_software/center/form.py
@@ -1,7 +1,11 @@
+import material
 from django import forms
-from  material import Layout,Row,Fieldset
-from center.models import Registration,UserImagesUpload
- # class RegistrationForm(forms.Form):
+from material import Layout, Row, Fieldset
+
+from center.models import Registration, UserImagesUpload
+
+
+# class RegistrationForm(forms.Form):
 #     username = forms.CharField()
 #     email = forms.EmailField(label="Email Address")
 #     password = forms.CharField(widget=forms.PasswordInput)
@@ -19,11 +23,65 @@ from center.models import Registration,UserImagesUpload
 #                                  'gender',
 #                                  'receive_news', 'agree_toc'))
 
-class Registration_model(forms.ModelForm):
+
+class Registration_model(forms.Form):
+    # photoimage = models.FileField(blank=True, null=True)
+    username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = Registration
-        fields = '__all__'
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm password")
+    firstname = forms.CharField(max_length=100)
+    lastname = forms.CharField(max_length=100)
+    sex = forms.ChoiceField(choices=((None, ''), ('F', 'Female'), ('M', 'Male'), ('O', 'Other')),required=False)
+    email = forms.EmailField(max_length=250)
+    fathername = forms.CharField(max_length=100)
+    joiningdate = forms.DateField()
+    dob = forms.DateField()
+    spousename = forms.CharField(max_length=100)
+    currentresidencyaddress = forms.CharField(max_length=100)
+    czip_code = forms.CharField(max_length=20)
+    cstate = forms.CharField(max_length=100)
+    ccountry = forms.CharField(max_length=100)
+    permanentaddress = forms.CharField(max_length=100)
+    pzip_code = forms.CharField(max_length=20)
+    pstate = forms.CharField(max_length=100)
+    pcountry = forms.CharField(max_length=100)
+    personalcontactno = forms.IntegerField()
+    emergencycontactno = forms.IntegerField()
+    highesteducation = forms.CharField(max_length=250)
+    educationalinstitutename = forms.CharField(max_length=250)
+    yearofpassing = forms.DateField()
+    previousemployeename = forms.CharField(max_length=250)
+    previousempoyeefrom = forms.DateField()
+    previousempoyeeto = forms.DateField()
+    # photoidproofimage = forms.FileField(blank=True, null=True)
+    panno = forms.CharField(max_length=250)
+    bankedtailsforsalary = forms.CharField(max_length=100)
+    activeuser = forms.BooleanField()
+    employeeinfo = forms.ChoiceField(choices=((None, ''), ('F', 'Full Time'), ('P', 'Part Time'), ('O', 'Other')),required=False)
+    agree_toc = forms.BooleanField(required=True, label='I agree with the Terms and Conditions')
+
+    layout = Layout('username', 'email',
+                    Row('password', 'password_confirm'),
+                    Fieldset('Personal details',
+                             Row('firstname', 'lastname'),
+                             Row('joiningdate', 'dob'),
+                             Row('personalcontactno', 'emergencycontactno'),
+                             Row('currentresidencyaddress', 'czip_code','cstate','ccountry'),
+                             Row('permanentaddress', 'pzip_code', 'pstate', 'pcountry'),
+                             Row('highesteducation', 'educationalinstitutename','yearofpassing'),
+                             Row('previousemployeename', 'previousempoyeefrom','previousempoyeeto'),
+                             'spousename',
+                             'sex','panno','bankedtailsforsalary','bankedtailsforsalary',
+                             'activeuser', 'agree_toc'))
+
+
+# class Registration_model(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput)
+#
+#     class Meta:
+#         model = Registration
+#         fields = '__all__'
+
 
 class UserImages_Form(forms.ModelForm):
     class Meta:
